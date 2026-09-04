70be34f02bb9426f499db8aa74dc580484f658bd
mariam khaled <khaldmrym769@gmail.com>
2026-09-04 22:14:53 +0300

    finally

--- prisma/migrations/20260904190000_azan_prefs_device_tokens/migration.sql
+++ prisma/migrations/20260904190000_azan_prefs_device_tokens/migration.sql
@@ -16,14 +16,3 @@ CREATE TABLE IF NOT EXISTS "device_tokens" (
 
 CREATE UNIQUE INDEX IF NOT EXISTS "device_tokens_token_key" ON "device_tokens"("token");
 CREATE INDEX IF NOT EXISTS "device_tokens_userId_idx" ON "device_tokens"("userId");
-
-DO $$
-BEGIN
-  IF NOT EXISTS (
-    SELECT 1 FROM pg_constraint WHERE conname = 'device_tokens_userId_fkey'
-  ) THEN
-    ALTER TABLE "device_tokens"
-      ADD CONSTRAINT "device_tokens_userId_fkey"
-      FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-  END IF;
-END $$;
